b817a34569078b6e2e8cf1bf142d34ea78efe0c4
kara <g22307@kagawa.kosen-ac.jp>
2023-07-29 15:55:21 +0900

    feat: mino can move right and left

--- src/board.rs
+++ src/board.rs
@@ -6,6 +6,7 @@ use termion::{clear, cursor};
 pub struct Board {
     pub board: [[u32; BOARD_COL]; BOARD_ROW],
     active_mino: Option<Mino>,
+    pub mino_x_dir: i32,
 }
 
 impl Board {
@@ -22,6 +23,7 @@ impl Board {
         Ok(Self {
             board,
             active_mino: None,
+            mino_x_dir: 0,
         })
     }
 
@@ -32,19 +34,33 @@ impl Board {
 
     pub fn update(&mut self) {
         if let Some(ref mut mino) = self.active_mino {
-            mino.pos.y += 1;
-
-            // detect overlap
-            for y in 0..BOARD_ROW {
-                for x in 0..BOARD_COL {
-                    let mino_pixel = mino.get_pixel(Int2::new(x as i32, y as i32));
-                    if mino_pixel != 0 && self.board[y][x] != 0 {
-                        mino.pos.y -= 1;
-                        mino.apply(&mut self.board);
-                        self.active_mino = None;
-                        return;
+            {
+                mino.pos.y += 1;
+                for y in 0..BOARD_ROW {
+                    for x in 0..BOARD_COL {
+                        let mino_pixel = mino.get_pixel(Int2::new(x as i32, y as i32));
+                        if mino_pixel != 0 && self.board[y][x] != 0 {
+                            mino.pos.y -= 1;
+                            mino.apply(&mut self.board);
+                            self.active_mino = None;
+                            return;
+                        }
                     }
                 }
+            }
+
+            {
+                mino.pos.x += self.mino_x_dir;
+                for y in 0..BOARD_ROW {
+                    for x in 0..BOARD_COL {
+                        let mino_pixel = mino.get_pixel(Int2::new(x as i32, y as i32));
+                        if mino_pixel != 0 && self.board[y][x] != 0 {
+                            mino.pos.x -= self.mino_x_dir;
+                            return;
+                        }
+                    }
+                }
+                self.mino_x_dir = 0;
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use std::io::{stdin, stdout};
-
 use anyhow::{Ok, Result};
 use board::Board;
 use mino::Mino;
+use std::{
+    collections::VecDeque,
+    io::{stdin, stdout},
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 use termion::{
     event::{Event, Key},
-    input::TermReadEventsAndRaw,
+    input::TermRead,
     raw::IntoRawMode,
+    screen::IntoAlternateScreen,
 };
+use tokio_util::sync::CancellationToken;
 
 mod board;
 mod mino;
@@ -16,7 +22,8 @@ mod vector;
 pub const BOARD_ROW: usize = 10;
 pub const BOARD_COL: usize = 10;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let minos = [Mino::new([
         [1, 1, 0, 0],
         [1, 1, 0, 0],
@@ -27,21 +34,50 @@ fn main() -> Result<()> {
     let mut board = Board::new()?;
 
     let stdin = stdin();
-    let mut stdout = stdout().into_raw_mode()?;
+    let mut stdout = stdout().into_raw_mode()?.into_alternate_screen()?;
+
+    let event_queue = Arc::new(Mutex::new(VecDeque::new()));
+    // key loop
+    let event_queue_clone = event_queue.clone();
+    let handle = tokio::spawn(async move {
+        for ele in stdin.events() {
+            let ele = ele.unwrap();
+
+            if ele == Event::Key(Key::Ctrl('c')) {
+                break;
+            } else {
+                let mut lock = event_queue_clone.lock().unwrap();
+                (*lock).push_front(ele);
+            }
+        }
+    });
 
     // main loop
     board.put_mino(minos[0].clone());
     board.draw(&mut stdout)?;
-    for ele in stdin.events_and_raw() {
-        let (ev, _) = ele?;
 
-        if ev == Event::Key(Key::Ctrl('c')) {
+    loop {
+        if handle.is_finished() {
             return Ok(());
         }
 
-        board.update();
-        board.draw(&mut stdout)?;
-    }
+        {
+            let mut events = event_queue.lock().unwrap();
+            while let Some(Event::Key(key)) = events.pop_back() {
+                match key {
+                    Key::Right => {
+                        board.mino_x_dir = 1;
+                    }
+                    Key::Left => {
+                        board.mino_x_dir = -1;
+                    }
+                    _ => {}
+                }
+            }
+        }
 
-    Ok(())
+        board.update();
+        board.draw(&mut stdout).unwrap();
+        tokio::time::sleep(Duration::from_millis(100)).await;
+    }
 }
